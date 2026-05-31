f9da397b066fa1cbb19fc68ab3929bf41cbdb6f0
knowledeagablejahil <s.a.bukhari208@gmail.com>
2026-05-31 19:28:39 +0400

    Restored original design and fixed navbar visibility

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,13 @@
+'use client'
+
 import '@/app/globals.css'
+import Navbar from '@/components/Navbar'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  )
 }
